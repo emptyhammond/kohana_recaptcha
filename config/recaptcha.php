c6187592019c7175dda5a297cd0dc9ffe7206d73
Mathew Hammond <emptyhammond@gmail.com>
2011-02-17 17:37:13 +0000

    Added config file

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -2,8 +2,8 @@
 
 return array(
 	'default'	=> array(
-		'publickey'		=>	'your public key here',
-		'privatekey' 	=>	'your private key here',
+		'publickey'		=>	null, 	//your public key here
+		'privatekey' 	=>	null,	//your private key here
 	),
 );
 //If you don't have keys yet go to www.google.com/recaptcha to sign up
